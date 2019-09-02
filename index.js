c89277ca8bff4a3be22e1d2f590ad3557c90d3ef
D Y <yomtovdaniel1@gmail.com>
2019-09-02 09:46:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
+  
 
-  return Object.assign({}, object, {[key]: value});
+  return Object.assign({}, object, {[key]: value });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
